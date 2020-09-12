a51037b728ccae1724be4669d5b6bc4995ba46fa
olyklohan <69878774+olyklohan@users.noreply.github.com>
2020-09-13 02:28:26 +0530

    Add files via upload

---
+++ geeksforgeeks_circle_image_view_onlyK_Lohan/geeksforgeeks/app/src/main/java/org/geeksforgeeks/project/MainActivity.java
@@ -0,0 +1,24 @@
+//MainActivity.java file
+
+package org.geeksforgeeks.project;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
